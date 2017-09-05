139d4bb8b00c2a385571234f1adccb99aec0f2fe
unknown <iskrzycki@gmail.com>
2017-09-05 21:05:46 +0200

    Rooms support added. Basic game implementation (not finished). UI changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,45 @@
 var socket = io();
 
-var elem = document.getElementById('myCanvas'),
-    context = elem.getContext('2d'),
-    elements = [],
-    elemBounding = elem.getBoundingClientRect(),
-    moveNumber = 0;
+var elem = document.getElementById('myCanvas');
+var context = elem.getContext('2d');
+var elemBounding;
+var elements = [];
+var moveNumber = 0;
+var playerName;
 
-    var CONFIG = {
-        BOARD: {
-            TILE_SIZE: 100,
-            TILE_COLOR: '#f1f1f1'
-        },
-        X_COLOR: '#ff0000',
-        O_COLOR: '#00ff00'
-    };
+var CONFIG = {
+    BOARD: {
+        TILE_SIZE: 100,
+        TILE_COLOR: '#f1f1f1'
+    },
+    X_COLOR: '#ff0000',
+    O_COLOR: '#00ff00'
+};
 
 document.querySelector("#nameForm").addEventListener("submit", function(e) {
+    e.preventDefault();
     var input = document.getElementById('playerNameInput');
     socket.emit('newPlayer', input.value);
+    playerName = input.value;
+    document.getElementById('nameForm').remove();
 });
 
 elem.addEventListener('click', function(event) {
+    elemBounding = elem.getBoundingClientRect();
     var x = event.pageX - elemBounding.left;
     var y = event.pageY - elemBounding.top;
 
     var selectedElem = getClickedElement(x, y);
     if (selectedElem) {
-        moveNumber++;
-        selectedElem.isBlank = false;
+        socket.emit('move', selectedElem);
+        // moveNumber++;
+        // selectedElem.isBlank = false;
 
-        if (moveNumber %2 == 0) {
-            socket.emit('draw', 'o', selectedElem);
-        } else {
-            socket.emit('draw', 'x', selectedElem);
-        }
+        // if (moveNumber %2 == 0) {
+        //     socket.emit('draw', 'o', selectedElem);
+        // } else {
+        //     socket.emit('draw', 'x', selectedElem);
+        // }
     }
 
 }, false);
@@ -46,6 +52,41 @@ socket.on('draw', function (data, elem) {
     }
 });
 
+socket.on('userCounter', function (count) {
+    document.getElementById('userCounter').innerHTML = "users online: " + count;
+});
+
+socket.on('startGame', function (data) {
+    displayBoard(true);
+    document.getElementById('roomName').innerHTML = data.roomName;
+    console.log('start game');
+});
+
+socket.on('symbol', function (data) {
+    console.log('Game started and my symbol is: ' + data.playerType);
+});
+
+socket.on('playerList', function (playerList) {
+    var ul = document.getElementById('player-list');
+
+    while(ul.firstChild) {
+        ul.removeChild(ul.firstChild);
+    }
+
+    for (var i = 0, len = playerList.length; i < len ; i++) {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(playerList[i]));
+        if (playerName !== playerList[i]) {
+            li.addEventListener("click", function(e) {
+                socket.emit('createRoom', e.srcElement.innerHTML);
+            }, false);
+        } else {
+            li.className += 'current-player';
+        }
+        ul.appendChild(li);
+    }
+});
+
 function getClickedElement (x, y) {
     for (var i = 0, len = elements.length; i < len ; i++) {
         if (y > elements[i].top && y < elements[i].top + CONFIG.BOARD.TILE_SIZE && x > elements[i].left && x < elements[i].left + CONFIG.BOARD.TILE_SIZE && elements[i].isBlank) {
@@ -55,15 +96,15 @@ function getClickedElement (x, y) {
 }
 
 // Add element.
-elements.push({top: 0, left: 0, id: 1, isBlank: true});
-elements.push({top: 0, left: 105, id: 2, isBlank: true});
-elements.push({top: 0, left: 210, id: 3, isBlank: true});
-elements.push({top: 105, left: 0, id: 4, isBlank: true});
-elements.push({top: 105, left: 105, id: 5, isBlank: true});
-elements.push({top: 105, left: 210, id: 6, isBlank: true});
-elements.push({top: 210, left: 0, id: 7, isBlank: true});
-elements.push({top: 210, left: 105, id: 8, isBlank: true});
-elements.push({top: 210, left: 210, id: 9, isBlank: true});
+elements.push({top: 0, left: 0, id: 0, isBlank: true});
+elements.push({top: 0, left: 105, id: 1, isBlank: true});
+elements.push({top: 0, left: 210, id: 2, isBlank: true});
+elements.push({top: 105, left: 0, id: 3, isBlank: true});
+elements.push({top: 105, left: 105, id: 4, isBlank: true});
+elements.push({top: 105, left: 210, id: 5, isBlank: true});
+elements.push({top: 210, left: 0, id: 6, isBlank: true});
+elements.push({top: 210, left: 105, id: 7, isBlank: true});
+elements.push({top: 210, left: 210, id: 8, isBlank: true});
 
 // Render elements.
 elements.forEach(function(element) {
@@ -99,4 +140,14 @@ function drawX(element) {
     context.strokeStyle = CONFIG.X_COLOR;
     context.stroke();
     context.closePath();
+}
+
+window.onbeforeunload = function() {
+    // TODO: Uncomment on prod
+    //return "Do you really want to refresh page? You'll be logged out.";
+}
+
+function displayBoard (isVisible) {
+    var displayStyle = isVisible ? 'block' : 'none';
+    document.getElementById('board').style.display = displayStyle;
 }
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 var port = 3000;
 
+var players = [];
+var allClients = [];
+
 app.use('/scripts', express.static(__dirname + '/scripts'));  
 app.use('/styles', express.static(__dirname + '/styles'));  
 
@@ -13,19 +16,66 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket) {
+    allClients.push(socket);
+    io.emit('userCounter', allClients.length);
+    io.emit('playerList', players);
     console.log('user connected', socket.id);
+    console.log('user count:', allClients.length);
 
-    socket.on('draw', function(data, elem) {
-        console.log('DRAW: ', data);
-        if (data === 'o') {
-            io.emit('draw', 'o', elem);
-        } else if (data === 'x') {
-            io.emit('draw', 'x', elem);
-        }
-    });
+    // socket.on('draw', function(data, elem) {
+    //     console.log('DRAW:', data, 'by:', socket.id);
+    //     if (data === 'o') {
+    //         io.emit('draw', 'o', elem);
+    //     } else if (data === 'x') {
+    //         io.emit('draw', 'x', elem);
+    //     }
+    // });
 
     socket.on('newPlayer', function(name) {
-        console.log('New player: ', name);        
+        socket.playerName = name;
+        console.log('New player: ', name);
+        players.push(name);
+        io.emit('playerList', players);
+    });
+
+    socket.on('createRoom', function(secondPlayerName) {
+        var roomName = socket.playerName + " vs " + secondPlayerName;
+        console.log('creating room', roomName);
+        
+        socket.join(roomName);
+        var secondPlayerSocket = allClients.filter(function(obj) {
+            return obj.playerName === secondPlayerName;
+        });
+        secondPlayerSocket[0].join(roomName);
+
+        var gameData = {
+            roomName: roomName
+        };
+
+        socket.symbol = 'x';
+        secondPlayerSocket[0].symbol = 'o';
+
+        io.to(roomName).emit('startGame', gameData);
+        io.to(socket.id).emit('symbol', {playerType:'x'});
+        io.to(secondPlayerSocket[0].id).emit('symbol', {playerType:'o'});
+    });
+
+    socket.on('move', function(elem) {
+        io.emit('draw', socket.symbol, elem);
+    });
+
+    socket.on('disconnect', function() {
+        var i = allClients.indexOf(socket);
+        var j = players.indexOf(allClients[i].playerName);
+        allClients.splice(i, 1);
+        if (j > -1) {
+            players.splice(j, 1);    
+        }
+        
+        io.emit('playerList', players);
+        io.emit('userCounter', allClients.length);
+        console.log('user count:', allClients.length);
+        console.log('player count:', players.length);
     });
 });
 
